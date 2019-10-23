307ec46f360ecae5d0edc580a9056efd417cc3d0
Daniel <daniel.rm99@hotmail.com>
2019-10-23 15:39:38 +0200

    Initial commit

---
+++ src/test/java/no/kristiania/jdbc/ProductDao.java
@@ -0,0 +1,16 @@
+package no.kristiania.jdbc;
+
+
+import org.assertj.core.util.Lists;
+
+public class ProductDao {
+    public void insertProduct(String apples) {
+
+    }
+
+    public Lists listAll() {
+
+
+
+    }
+}
---
+++ src/test/java/no/kristiania/jdbc/WebshopTest.java
@@ -0,0 +1,15 @@
+package no.kristiania.jdbc;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class WebshopTest {
+    @Test
+    void shouldRetrieveStoresProduct() {
+        ProductDao dao = new ProductDao();
+        dao.insertProduct("Apples");
+        assertThat(dao.listAll().contains("Apples");
+
+    }
+}
